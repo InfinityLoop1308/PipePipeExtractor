c310d67b29cc6c7445f04f9bf638b157078e707c
kedwjao <96324692+kedwjao@users.noreply.github.com>
2022-07-03 13:26:36 +0800

    feat: support more search types(BiliBili)

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/bilibili/extractors/BilibiliSearchExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/bilibili/extractors/BilibiliSearchExtractor.java
@@ -53,66 +53,48 @@ public class BilibiliSearchExtractor extends SearchExtractor{
 
     @Override
     public InfoItemsPage<InfoItem> getInitialPage() throws IOException, ExtractionException {
-        JsonArray result_videos = searchCollection.getObject("data").getArray("result").getObject(10).getArray("data");
-        JsonArray result_users = searchCollection.getObject("data").getArray("result").getObject(2).getArray("data");
-        JsonArray result = searchCollection.getObject("data").getArray("result");
-        if(result_videos.size() + result_users.size() == 0){
-            return new InfoItemsPage<>(new MultiInfoItemsCollector(getServiceId()), null);
-        }
-        for(int i=0;i<result.size();i++){
-            if(result.getObject(i).getString("result_type").equals("bili_user")){
-                result_users = searchCollection.getObject("data").getArray("result").getObject(i).getArray("data");
-            }
-        }
-
-        final MultiInfoItemsCollector collector = new MultiInfoItemsCollector(getServiceId());
-        for (int i = 0; i< result_users.size(); i++){
-            collector.commit(new BilibiliSearchResultChannelInfoItemExtractor(result_users.getObject(i)));
-        }
-        for (int i = 0; i< result_videos.size(); i++){
-            collector.commit(new BilibiliStreamInfoItemExtractor(result_videos.getObject(i)));
-        }
         int currentPage = 1;
         String nextPage = getUrl().replace(String.format("page=%s", 1), String.format("page=%s", String.valueOf(currentPage + 1)));
-        return new InfoItemsPage<>(collector, new Page(nextPage));
+        return new InfoItemsPage<>(getCommittedCollector(), new Page(nextPage));
+    }
+
+    private MultiInfoItemsCollector getCommittedCollector(){
+        JsonArray result = searchCollection.getObject("data").getArray("result");
+        final MultiInfoItemsCollector collector = new MultiInfoItemsCollector(getServiceId());
+        for (int i = 0; i< result.size(); i++) {
+            String type = result.getObject(i).getString("type");
+            switch (type){
+                case "video":
+                    collector.commit(new BilibiliStreamInfoItemExtractor(result.getObject(i)));
+                    break;
+                case "live_room":
+                    collector.commit(new BilibiliLiveInfoItemExtractor(result.getObject(i)));
+                    break;
+                case "bili_user":
+                    collector.commit(new BilibiliSearchResultChannelInfoItemExtractor(result.getObject(i)));
+            }
+        }
+        return collector;
     }
 
     @Override
     public InfoItemsPage<InfoItem> getPage(Page page) throws IOException, ExtractionException {
         final String html = getDownloader().get(page.getUrl()).responseBody();
 
-        
-
         try {
             searchCollection = JsonParser.object().from(html);
         } catch (JsonParserException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        JsonArray result_users = searchCollection.getObject("data").getArray("result").getObject(2).getArray("data");
-        JsonArray result_videos = searchCollection.getObject("data").getArray("result").getObject(10).getArray("data");
-        if(result_videos.size() == 0){
-            return new InfoItemsPage<>(new MultiInfoItemsCollector(getServiceId()), null);
-        }
-        final MultiInfoItemsCollector collector = new MultiInfoItemsCollector(getServiceId());
-//        for (int i = 0; i< result_users.size(); i++){
-//            collector.commit(new BilibiliSearchResultChannelInfoItemExtractor(result_users.getObject(i)));
-//        }
-        for (int i = 0; i< result_videos.size(); i++){
-            collector.commit(new BilibiliStreamInfoItemExtractor(result_videos.getObject(i)));
-        }
-
 
         String currentPageString = page.getUrl().split("page=")[page.getUrl().split("page=").length-1];
         int currentPage = Integer.parseInt(currentPageString);
         String nextPage = page.getUrl().replace(String.format("page=%s", currentPageString), String.format("page=%s", String.valueOf(currentPage + 1)));
-        return new InfoItemsPage<>(collector, new Page(nextPage));
+        return new InfoItemsPage<>(getCommittedCollector(), new Page(nextPage));
     }
 
     @Override
     public void onFetchPage(Downloader downloader) throws IOException, ExtractionException {
-        // TODO Auto-generated method stub
         final String response = getDownloader().get(
             getLinkHandler().getUrl()).responseBody();
         try {
--- extractor/src/main/java/org/schabi/newpipe/extractor/services/bilibili/linkHandler/BilibiliSearchQueryHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/bilibili/linkHandler/BilibiliSearchQueryHandlerFactory.java
@@ -2,6 +2,8 @@
 
 package org.schabi.newpipe.extractor.services.bilibili.linkHandler;
 
+import static org.schabi.newpipe.extractor.utils.Utils.UTF_8;
+
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
 
@@ -12,15 +14,42 @@ import java.util.List;
 public class BilibiliSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
 
 
+    private static final String VIDEOS = "video";
+    private static final String LIVE = "live_room";
+    private static final String USER = "bili_user";
+    private static final String SEARCH_URL = "https://api.bilibili.com/x/web-interface/search/type?search_type=";
+
     @Override
-    public String getUrl(final String query, final List<String> contentFilter, final String sortFilter)
+    public String getUrl(final String query, final List<String> contentFilters, final String sortFilter)
             throws ParsingException {
         try {
-            return "https://api.bilibili.com/x/web-interface/search/all/v2" + "?keyword=" +
-                    URLEncoder.encode(query, "UTF-8") + "&page=1";
+            if (!contentFilters.isEmpty()) {
+                final String contentFilter = contentFilters.get(0);
+                final String searchString = query;
+                switch (contentFilter) {
+                    case VIDEOS:
+                    default:
+                        return SEARCH_URL + VIDEOS + "&keyword=" + URLEncoder.encode(searchString, UTF_8) + "&page=1";
+                    case LIVE:
+                        return SEARCH_URL + LIVE + "&keyword=" + URLEncoder.encode(searchString, UTF_8) + "&page=1";
+                    case USER:
+                        return SEARCH_URL + USER + "&keyword=" + URLEncoder.encode(searchString, UTF_8) + "&page=1";
+                }
+            }
+
+            return SEARCH_URL + VIDEOS + "&keyword=" + URLEncoder.encode(query, UTF_8) + "&page=1";
 
         } catch (final UnsupportedEncodingException e) {
             throw new ParsingException("query \"" + query + "\" could not be encoded", e);
         }
+    }
+
+    @Override
+    public String[] getAvailableContentFilter() {
+        return new String[]{
+                VIDEOS,
+                LIVE,
+                USER
+        };
     }
 }
