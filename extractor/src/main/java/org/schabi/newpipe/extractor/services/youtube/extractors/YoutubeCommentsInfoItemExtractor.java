e8e32f2999a36c3664d661eb4dd8c46828e918eb
xz-dev <xiangzhedev@gmail.com>
2022-09-27 03:22:52 +0800

    Use UNKNOWN_REPLY_COUNT everywhere

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeCommentsInfoItemExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeCommentsInfoItemExtractor.java
@@ -1,5 +1,6 @@
 package org.schabi.newpipe.extractor.services.youtube.extractors;
 
+import static org.schabi.newpipe.extractor.comments.CommentsInfoItem.UNKNOWN_REPLY_COUNT;
 import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.getTextFromObject;
 import static org.schabi.newpipe.extractor.utils.Utils.EMPTY_STRING;
 
@@ -254,7 +255,7 @@ public class YoutubeCommentsInfoItemExtractor implements CommentsInfoItemExtract
         try {
             return JsonUtils.getNumber(json, "comment.commentRenderer.replyCount").intValue();
         } catch (final Exception e) {
-            return -1;
+            return UNKNOWN_REPLY_COUNT;
         }
     }
 
