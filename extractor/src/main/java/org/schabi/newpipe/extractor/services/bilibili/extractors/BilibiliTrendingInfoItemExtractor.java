64034e2a057499aaab5538456ed9b34425bade41
Infinity1309 <96324692+InfinityLoop1309@users.noreply.github.com>
2023-01-05 07:21:06 +0800

    feat: support Top 100 Kiosk(BiliBili)

---
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/bilibili/extractors/BilibiliTrendingInfoItemExtractor.java
@@ -0,0 +1,89 @@
+package org.schabi.newpipe.extractor.services.bilibili.extractors;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import com.grack.nanojson.JsonObject;
+
+import org.schabi.newpipe.extractor.exceptions.ParsingException;
+import org.schabi.newpipe.extractor.localization.DateWrapper;
+import org.schabi.newpipe.extractor.stream.StreamInfoItemExtractor;
+import org.schabi.newpipe.extractor.stream.StreamType;
+
+public class BilibiliTrendingInfoItemExtractor implements StreamInfoItemExtractor{
+
+    protected final JsonObject item;
+    public BilibiliTrendingInfoItemExtractor(final JsonObject json) {
+        item = json;
+    }
+
+    @Override
+    public String getName() throws ParsingException {
+        return item.getString("title");
+    }
+
+    @Override
+    public String getUrl() throws ParsingException {
+        return "https://bilibili.com/" + item.getString("bvid");
+    }
+
+    @Override
+    public String getThumbnailUrl() throws ParsingException {
+        return item.getString("pic").replace("http:", "https:");
+    }
+
+    @Override
+    public StreamType getStreamType() throws ParsingException {
+        return StreamType.VIDEO_STREAM;
+    }
+
+    @Override
+    public boolean isAd() throws ParsingException {
+        return false;
+    }
+
+    @Override
+    public long getDuration() throws ParsingException {
+        return item.getInt("duration");
+    }
+
+    @Override
+    public long getViewCount() throws ParsingException {
+        return item.getObject("stat").getInt("view");
+    }
+
+    @Override
+    public String getUploaderName() throws ParsingException {
+        return item.getObject("owner").getString("name");
+    }
+
+    @Override
+    public String getUploaderUrl() throws ParsingException {
+        return null;
+    }
+
+    @Override
+    public String getUploaderAvatarUrl() throws ParsingException {
+        return item.getObject("owner").getString("face");
+    }
+
+    @Override
+    public boolean isUploaderVerified() throws ParsingException {
+        return false;
+    }
+
+    @Override
+    public String getTextualUploadDate() throws ParsingException {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(item.getInt("pubdate") * 1000L));
+    }
+
+    @Override
+    public DateWrapper getUploadDate() throws ParsingException {
+        return new DateWrapper(LocalDateTime.parse(
+                getTextualUploadDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).atOffset(ZoneOffset.ofHours(+8)));
+    }
+
+}
