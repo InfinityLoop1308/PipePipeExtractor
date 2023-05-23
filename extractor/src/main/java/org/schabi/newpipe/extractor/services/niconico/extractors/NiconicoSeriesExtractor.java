fade9e1258a5ef66e40f21df86f2ac2522902e6d
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2023-05-23 15:08:11 +0800

    fix: some series can not be extracted (NicoNico)  [CodeBerg #35]

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/niconico/extractors/NiconicoSeriesExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/niconico/extractors/NiconicoSeriesExtractor.java
@@ -1,5 +1,7 @@
 package org.schabi.newpipe.extractor.services.niconico.extractors;
 
+import com.grack.nanojson.JsonArray;
+import com.grack.nanojson.JsonParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -25,6 +27,7 @@ public class NiconicoSeriesExtractor extends PlaylistExtractor {
     private String avatar;
     private int count;
     private String name;
+    private int type = 0;
 
     public NiconicoSeriesExtractor(StreamingService service, ListLinkHandler linkHandler) {
         super(service, linkHandler);
@@ -33,12 +36,22 @@ public class NiconicoSeriesExtractor extends PlaylistExtractor {
     @Override
     public void onFetchPage(@Nonnull Downloader downloader) throws IOException, ExtractionException {
         Document response = Jsoup.parse(getDownloader().get(getLinkHandler().getUrl(), Localization.DEFAULT).responseBody());
-        uploaderName = response.select(".SeriesAdditionalContainer-ownerName").text();
-        uploaderUrl = response.select(".SeriesAdditionalContainer-ownerName").attr("href");
-        avatar = response.select(".UserIcon-image").attr("src");
-        count = Integer.parseInt(response.select(".SeriesDetailContainer-bodyMeta").text().split(" video")[0].split("Total ")[1]);
-        name = response.select(".SeriesDetailContainer-bodyTitle").text();
-        data = response.select("div.NC-MediaObject.NC-VideoMediaObject.SeriesVideoListContainer-video > div.NC-MediaObject-main");
+        if(response.select(".SeriesAdditionalContainer-ownerName").isEmpty()){
+            type = 1;
+            uploaderName = response.select("meta[property=profile:username]").attr("content");
+            uploaderUrl = response.select("meta[property=og:url]").attr("content").split("/series/")[0];
+            avatar = response.select("meta[property=og:image]").attr("content");
+            count = Integer.parseInt(response.select("meta[property=og:title]").attr("content").split("（全")[1].split("件）")[0]);
+            name = response.select("meta[property=og:description]").attr("content").split("の「")[1].split("（全")[0];
+            data = response.select("script[type=application/ld+json]");
+        } else {
+            uploaderName = response.select(".SeriesAdditionalContainer-ownerName").text();
+            uploaderUrl = response.select(".SeriesAdditionalContainer-ownerName").attr("href");
+            avatar = response.select(".UserIcon-image").attr("src");
+            count = Integer.parseInt(response.select(".SeriesDetailContainer-bodyMeta").text().split(" video")[0].split("Total ")[1]);
+            name = response.select(".SeriesDetailContainer-bodyTitle").text();
+            data = response.select("div.NC-MediaObject.NC-VideoMediaObject.SeriesVideoListContainer-video > div.NC-MediaObject-main");
+        }
     }
 
     @Nonnull
@@ -51,9 +64,22 @@ public class NiconicoSeriesExtractor extends PlaylistExtractor {
     @Override
     public InfoItemsPage<StreamInfoItem> getInitialPage() throws IOException, ExtractionException {
         final StreamInfoItemsCollector collector = new StreamInfoItemsCollector(getServiceId());
-        for(Element element: data){
-            collector.commit(new NiconicoSeriesContentItemExtractor(element, uploaderUrl, uploaderName));
+
+        if(type == 1) {
+            try {
+                JsonArray array = JsonParser.object().from(data.html()).getArray("itemListElement");
+                for (int i = 0; i < array.size(); i++) {
+                    collector.commit(new NiconicoSeriesJSONContentItemExtractor(array.getObject(i), uploaderName, uploaderUrl, avatar));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            for(Element element: data){
+                collector.commit(new NiconicoSeriesContentItemExtractor(element, uploaderUrl, uploaderName));
+            }
         }
+
         return new InfoItemsPage<>(collector, null);
     }
 
