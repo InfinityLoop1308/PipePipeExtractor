7c95b5ed8549fd1f0193092a9be9c979f5a6dea5
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2023-06-08 16:29:16 +0800

    fix: HTML strings in search results are not decoded (BiliBili)

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/bilibili/extractors/BilibiliStreamInfoItemExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/bilibili/extractors/BilibiliStreamInfoItemExtractor.java
@@ -13,6 +13,7 @@ import com.grack.nanojson.JsonObject;
 import com.grack.nanojson.JsonParser;
 import com.grack.nanojson.JsonParserException;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.jsoup.nodes.Element;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.localization.DateWrapper;
@@ -31,7 +32,10 @@ public class BilibiliStreamInfoItemExtractor implements StreamInfoItemExtractor 
 
     @Override
     public String getName() throws ParsingException {
-        return item.getString("title").replace("<em class=\"keyword\">", "").replace("</em>", "");
+        return StringEscapeUtils.unescapeHtml4(
+                item.getString("title")
+                        .replace("<em class=\"keyword\">", "")
+                        .replace("</em>", ""));
     }
 
     @Override
