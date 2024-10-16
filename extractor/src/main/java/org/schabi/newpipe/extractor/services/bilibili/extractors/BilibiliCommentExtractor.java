005827b04e133ed1d10fc021a9547f150bab004e
chr56 <chr0056@gmail.com>
2024-10-16 09:48:54 +0800

    fix: only hot comments are loaded (BiliBili)

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/bilibili/extractors/BilibiliCommentExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/bilibili/extractors/BilibiliCommentExtractor.java
@@ -4,6 +4,7 @@ import com.grack.nanojson.JsonArray;
 import com.grack.nanojson.JsonObject;
 import com.grack.nanojson.JsonParser;
 import com.grack.nanojson.JsonParserException;
+
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.schabi.newpipe.extractor.Page;
 import org.schabi.newpipe.extractor.StreamingService;
@@ -16,6 +17,7 @@ import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
 import org.schabi.newpipe.extractor.services.bilibili.utils;
 
 import javax.annotation.Nonnull;
+
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -32,7 +34,10 @@ public class BilibiliCommentExtractor extends CommentsExtractor {
     @Override
     public void onFetchPage(@Nonnull Downloader downloader) throws IOException, ExtractionException {
         try {
-            String response = downloader.get(getUrl(), getHeaders()).responseBody();
+            String url = getUrl();
+            String response = isRepliesOfComment(url)
+                    ? getDownloader().get(url, getHeaders()).responseBody()
+                    : getDownloader().get(url).responseBody();
             data = JsonParser.object().from(response);
             data = data.getObject("data");
         } catch (JsonParserException e) {
@@ -48,24 +53,30 @@ public class BilibiliCommentExtractor extends CommentsExtractor {
 
     @Override
     public InfoItemsPage<CommentsInfoItem> getPage(Page page) throws IOException, ExtractionException {
+        String pageUrl = page.getUrl();
+        String initialUrl = getUrl();
+
         JsonArray results;
-        if (page.getUrl().equals(getUrl())) {
+        if (pageUrl.equals(initialUrl)) { // first page with top comments
             results = data.getArray("top_replies");
-            for(int i = 0; i < results.size(); i++){
+            for (int i = 0; i < results.size(); i++) {
                 results.getObject(i).put("isTop", true);
             }
             results.addAll(data.getArray("replies"));
-        } else {
+        } else { // other pages
             try {
-                final String html = getDownloader().get(page.getUrl(), getHeaders()).responseBody();
-                data = JsonParser.object().from(html).getObject("data");
+                final String responseJson =
+                        isRepliesOfComment(pageUrl)
+                                ? getDownloader().get(pageUrl, getHeaders()).responseBody()
+                                : getDownloader().get(pageUrl).responseBody();
+                data = JsonParser.object().from(responseJson).getObject("data");
             } catch (JsonParserException e) {
                 e.printStackTrace();
             }
             results = data.getArray("replies");
         }
 
-        if (results == null || results.size() == 0) {
+        if (results == null || results.isEmpty()) { // empty page
             return new InfoItemsPage<>(new CommentsInfoItemsCollector(getServiceId()), null);
         }
 
@@ -74,31 +85,39 @@ public class BilibiliCommentExtractor extends CommentsExtractor {
             collector.commit(new BilibiliCommentsInfoItemExtractor(results.getObject(i)));
         }
 
-        if (page.getUrl().contains(FETCH_COMMENTS_URL)) { //comments
+        if (!isRepliesOfComment(pageUrl)) { //comments
             if (data.getObject("cursor").getBoolean("is_end")) {
                 return new InfoItemsPage<>(collector, null);
             }
             //data.cursor.pagination_reply.next_offset
             String offset = data.getObject("cursor").getObject("pagination_reply").getString("next_offset");
-            return new InfoItemsPage<>(collector, new Page(utils.getWbiResult(FETCH_COMMENTS_URL, new HashMap<String, String>(){{
-                put("oid", getUrl().split("oid=")[1].split("&")[0]);
-                put("type", "1");
-                put("mode", "3");
-                put("pagination_str", "{\"offset\":\"" + StringEscapeUtils.escapeJson(offset) + "\"}");
-                put("plat", "1");
-                put("web_location", "1315875");
-            }})));
+            return new InfoItemsPage<>(collector, new Page(utils.getWbiResult(FETCH_COMMENTS_URL, buildNextPageParam(initialUrl, offset))));
         } else { //replies
-            if (19 > results.size() && page.getUrl().contains("pn=1")) {
+            if (19 > results.size() && pageUrl.contains("pn=1")) {
                 return new InfoItemsPage<>(collector, null);
             }
-            return new InfoItemsPage<>(collector, new Page(utils.getNextPageFromCurrentUrl(page.getUrl(), "pn", 1)));
+            return new InfoItemsPage<>(collector, new Page(utils.getNextPageFromCurrentUrl(pageUrl, "pn", 1)));
         }
     }
 
     @Override
     public boolean isCommentsDisabled() throws ExtractionException {
         return getId().equals("LIVE");
+    }
+
+    private static boolean isRepliesOfComment(String requestUrl) {
+        return !requestUrl.contains(FETCH_COMMENTS_URL);
+    }
+
+    private static HashMap<String, String> buildNextPageParam(String url, String offset) {
+        return new HashMap<String, String>() {{
+            put("oid", url.split("oid=")[1].split("&")[0]);
+            put("type", "1");
+            put("mode", "3");
+            put("pagination_str", "{\"offset\":\"" + StringEscapeUtils.escapeJson(offset) + "\"}");
+            put("plat", "1");
+            put("web_location", "1315875");
+        }};
     }
 
 }
