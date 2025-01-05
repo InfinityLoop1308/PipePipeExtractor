0047f9f7194c9aca461ad9c0e4633c94bfb9b8d1
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2025-01-06 07:11:46 +0800

    fix: unexpected exception when blocking

--- extractor/src/main/java/org/schabi/newpipe/extractor/InfoItemsCollector.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/InfoItemsCollector.java
@@ -125,7 +125,7 @@ public abstract class InfoItemsCollector<I extends InfoItem, E extends InfoItemE
             // Only check channels if we haven't already marked for removal
             if (!shouldRemove) {
                 for (String channel : channels) {
-                    if (item instanceof StreamInfoItem &&
+                    if (item instanceof StreamInfoItem && ((StreamInfoItem) item).getUploaderName() != null &&
                             ((StreamInfoItem) item).getUploaderName().equals(channel)) {
                         shouldRemove = true;
                         break;  // No need to check other channels
