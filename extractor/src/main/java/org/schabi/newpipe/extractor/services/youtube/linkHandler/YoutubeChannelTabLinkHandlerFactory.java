29601fd46f9dce953ade1f9a8d0650facc00b5a6
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2026-05-27 17:57:44 +0800

    Revert "Reapply "Reapply "feat(youtube): support channel tab sorting"""
    This reverts commit 3e06f36ec130ca452dcf107f223eedbc30a07dc2.

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/linkHandler/YoutubeChannelTabLinkHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/linkHandler/YoutubeChannelTabLinkHandlerFactory.java
@@ -3,41 +3,19 @@ package org.schabi.newpipe.extractor.services.youtube.linkHandler;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.ChannelTabs;
 import org.schabi.newpipe.extractor.linkhandler.ListLinkHandlerFactory;
-import org.schabi.newpipe.extractor.search.filter.Filter;
-import org.schabi.newpipe.extractor.search.filter.FilterGroup;
 import org.schabi.newpipe.extractor.search.filter.FilterItem;
+import org.schabi.newpipe.extractor.services.youtube.search.filter.YoutubeFilters;
 
 import java.util.List;
 
 import javax.annotation.Nonnull;
 
 public final class YoutubeChannelTabLinkHandlerFactory extends ListLinkHandlerFactory {
-    public static final String SORT_LATEST = "latest";
-    public static final String SORT_POPULAR = "popular";
-    public static final String SORT_OLDEST = "oldest";
-
     private static final YoutubeChannelTabLinkHandlerFactory INSTANCE =
             new YoutubeChannelTabLinkHandlerFactory();
-    private final FilterGroup.Builder filterBuilder;
-    private final Filter availableSortFilter;
+    private final YoutubeFilters searchFilters = new YoutubeFilters();
 
     private YoutubeChannelTabLinkHandlerFactory() {
-        filterBuilder = new FilterGroup.Builder();
-
-        final int latestFilterId = filterBuilder.addSortItem(
-                new FilterItem(Filter.ITEM_IDENTIFIER_UNKNOWN, SORT_LATEST));
-        final int popularFilterId = filterBuilder.addSortItem(
-                new FilterItem(Filter.ITEM_IDENTIFIER_UNKNOWN, SORT_POPULAR));
-        final int oldestFilterId = filterBuilder.addSortItem(
-                new FilterItem(Filter.ITEM_IDENTIFIER_UNKNOWN, SORT_OLDEST));
-
-        availableSortFilter = new Filter.Builder(new FilterGroup[]{
-                filterBuilder.createSortGroup("sortby", true, new FilterItem[]{
-                        filterBuilder.getFilterForId(latestFilterId),
-                        filterBuilder.getFilterForId(popularFilterId),
-                        filterBuilder.getFilterForId(oldestFilterId),
-                })
-        }).build();
     }
 
     public static YoutubeChannelTabLinkHandlerFactory getInstance() {
@@ -63,46 +41,10 @@ public final class YoutubeChannelTabLinkHandlerFactory extends ListLinkHandlerFa
     }
 
     @Override
-    public String getUrl(final String id,
-                         @Nonnull final List<FilterItem> selectedContentFilter,
+    public String getUrl(final String id,@Nonnull final List<FilterItem> selectedContentFilter,
                          final List<FilterItem> selectedSortFilter)
             throws ParsingException {
-        return "https://www.youtube.com/" + id
-                + getUrlSuffix(selectedContentFilter.get(0).getName())
-                + getSortUrlQuery(selectedSortFilter);
-    }
-
-    private static String getSortUrlQuery(final List<FilterItem> selectedSortFilter)
-            throws ParsingException {
-        if (selectedSortFilter == null || selectedSortFilter.isEmpty()) {
-            return "";
-        }
-
-        final String selectedSortFilterName = selectedSortFilter.get(0).getName();
-        switch (selectedSortFilterName) {
-            case SORT_LATEST:
-            case SORT_POPULAR:
-            case SORT_OLDEST:
-                return "?sort=" + selectedSortFilterName;
-            default:
-                throw new ParsingException("Unsupported YouTube channel tab sort filter: "
-                        + selectedSortFilterName);
-        }
-    }
-
-    @Override
-    public Filter getAvailableSortFilter() {
-        return availableSortFilter;
-    }
-
-    @Override
-    public Filter getContentFilterSortFilterVariant(final int contentFilterId) {
-        return availableSortFilter;
-    }
-
-    @Override
-    public FilterItem getFilterItem(final int filterId) {
-        return filterBuilder.getFilterForId(filterId);
+        return "https://www.youtube.com/" + id + getUrlSuffix(selectedContentFilter.get(0).getName());
     }
 
     @Override
