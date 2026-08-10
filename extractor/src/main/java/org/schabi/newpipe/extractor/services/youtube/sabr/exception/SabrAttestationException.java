739fc6bd6b56fed7c7a88b347cd44ce525ac7271
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2026-08-10 20:42:50 +0800

    21

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/sabr/exception/SabrAttestationException.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/sabr/exception/SabrAttestationException.java
@@ -5,4 +5,8 @@ public final class SabrAttestationException extends SabrProtocolException {
     public SabrAttestationException(final String message) {
         super(message);
     }
+
+    public SabrAttestationException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }
